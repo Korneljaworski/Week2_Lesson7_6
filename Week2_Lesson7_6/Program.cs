8c9d6c9f6953d7bbf81ebe56bacfe22e8baae051
Korjaw Korjaw <kornel.jaworski@gmail.com>
2023-07-28 01:40:27 +0200

    Add project files.

---
+++ Week2_Lesson7_6/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+            Napisz program w C#, który pobierze wzrost użytkownika i przypisze mu wymyśloną kategorię wzrostu.
+            Dane testowe : 140
+            Rezultat w terminalu : Jesteś krasnoludem
+        */
+
+        Console.WriteLine("Podaj ile masz wzrostu:");
+
+        int userHeight = Int32.Parse(Console.ReadLine());
+
+        if (userHeight >= 200)
+        {
+            Console.WriteLine("Jesteś żyrafą.");
+        }
+        else if (userHeight >= 150)
+        {
+            Console.WriteLine("Jesteś człowiekiem.");
+        }
+        else if (userHeight >= 120)
+        {
+            Console.WriteLine("Jesteś krasnoludem.");
+        }
+        else
+        {
+            Console.WriteLine("Jesteś karliczkiem.");
+        }
+    }
+
+}
